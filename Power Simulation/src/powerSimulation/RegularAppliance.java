f65df691436f878b5c263b492c52d77ff37bfd60
LaurenceT123 <84556312+LaurenceT123@users.noreply.github.com>
2022-11-30 20:34:57 -0800

    Update RegularAppliance.java

--- Power Simulation/src/powerSimulation/RegularAppliance.java
+++ Power Simulation/src/powerSimulation/RegularAppliance.java
@@ -1,5 +1,16 @@
 package powerSimulation;
 
+	/* TODO:
+	Attributes:
+		private int ID; //keeps track of id of appliance
+		
+	Methods:
+		RegularAppliance() <--- takes in super class attributes + Id
+		Getters and Setters of attribute
+		toString method
+
+	
+	*/
 public class RegularAppliance extends Appliance{
 	RegularAppliance() {
 		//stuff
